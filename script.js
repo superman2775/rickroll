fddb6474f194d77e8f4ccb4fd1822ea23bbf1d8d
Super_man2775 <da.superman2775@gmail.com>
2026-05-31 10:48:38 +0200

    fix some bugs

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
+//gif spam
 setInterval(() => {
     const img = document.createElement('img');
     img.src = 'https://media.giphy.com/media/Vuw9m5wXviFIQ/giphy.gif';
+    img.style.position = 'absolute';
     img.style.left = Math.random() * (window.innerWidth - 200) + 'px';
     img.style.top = Math.random() * (window.innerHeight - 150) + 'px';
     document.body.appendChild(img);
 }, 250);
 
-let rickrollSong = new Audio('/music/rickroll-song.mp3');
-rickrollSong.play();
+//audio
+const rickrollSong = new Audio('/music/rickroll-song.mp3');
+const startAudio = () => {
+    rickrollSong.play();
+    document.removeEventListener('click', startAudio);
+    document.removeEventListener('keydown', startAudio);
+};
+    document.addEventListener('click', startAudio);
+    document.addEventListener('keydown', startAudio);
